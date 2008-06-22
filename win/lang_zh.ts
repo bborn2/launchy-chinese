13d03cc04914060680d1f7f495a59b9421779cc9
bborn.pisces <bborn.pisces@e224aad8-4150-0410-9b3f-8967f03d73e8>
2008-06-22 16:04:58 +0000

    create

---
+++ win/lang_zh.ts
@@ -0,0 +1,414 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="zh_CN">
+<defaultcodec></defaultcodec>
+<context>
+    <name>MyWidget</name>
+    <message>
+        <location filename="src/main.cpp" line="81"/>
+        <source>Launchy Options</source>
+        <translation>Launchy 选项</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="86"/>
+        <source>Close Launchy</source>
+        <translation>关闭 Launchy</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="644"/>
+        <source>A new version of Launchy is available</source>
+        <translation>Launchy 有新版本了！</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="645"/>
+        <source>A new version of Launchy is available.
+
+You can download it at http://www.launchy.net/</source>
+        <translation>Launchy 有新版本了！
+
+请到这里下载： http://www.launchy.net/</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="1000"/>
+        <source>Options</source>
+        <translation>选项</translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="1002"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDlg</name>
+    <message>
+        <location filename="src/options.cpp" line="56"/>
+        <source>Alt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="56"/>
+        <source>Win</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="56"/>
+        <source>Shift</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="56"/>
+        <source>Control</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="59"/>
+        <source>Double</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="59"/>
+        <source>Space</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="59"/>
+        <source>Tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="59"/>
+        <source>Backspace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="59"/>
+        <source>Enter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="59"/>
+        <source>Esc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="60"/>
+        <source>Home</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="60"/>
+        <source>End</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="60"/>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="60"/>
+        <source>Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="60"/>
+        <source>Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="60"/>
+        <source>Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="60"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="60"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F6</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F7</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="61"/>
+        <source>F9</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="62"/>
+        <source>F10</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="62"/>
+        <source>F11</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="62"/>
+        <source>F12</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="62"/>
+        <source>F13</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="346"/>
+        <source>Index has </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="346"/>
+        <source> items</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.cpp" line="412"/>
+        <source>Select a directory</source>
+        <translation>选择一个目录</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="17"/>
+        <source>Launchy options</source>
+        <translation>Launchy 选项</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="30"/>
+        <source>General</source>
+        <translation>常规</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="40"/>
+        <source>User Interface</source>
+        <translation>显示</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="46"/>
+        <source>Always display Launchy window</source>
+        <translation>总是显示主窗口</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="53"/>
+        <source>Hide Launchy when it loses focus</source>
+        <translation>当失去焦点时隐藏主窗口</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="63"/>
+        <source>Always on top</source>
+        <translation>总在最前面</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="70"/>
+        <source>Condense suggestion list</source>
+        <translation>精简建议列表</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="77"/>
+        <source>Always center Launchy</source>
+        <translation>总是居中主窗口</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="86"/>
+        <source>Hotkey:</source>
+        <translation>快捷键:</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="158"/>
+        <source>GUI</source>
+        <translation>界面</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="168"/>
+        <source>Opaqueness</source>
+        <translation>透明度</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="175"/>
+        <source>Fade in time</source>
+        <translation>淡入时间</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="182"/>
+        <source>Fade out time</source>
+        <translation>淡出时间</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="238"/>
+        <source>Simultaneously visible alternate suggestions</source>
+        <translation>一次显示建议列表的数目</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="285"/>
+        <source>Max. number of alternate suggestions</source>
+        <translation>最多显示建议列表的数目</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="337"/>
+        <source>System Options</source>
+        <translation>系统选项</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="343"/>
+        <source>Check for updates on startup</source>
+        <translation>启动的时候检测是否有新版本</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="350"/>
+        <source>Show hidden files in browse mode</source>
+        <translation>显示隐藏的文件</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="357"/>
+        <source>Portable mode (USB stick)</source>
+        <translation>移动模式（貌似可放到U盘中方便使用）</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="367"/>
+        <source>Use testing indexer?</source>
+        <translation>使用测试版的索引?</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="376"/>
+        <source>Minutes between catalog updates</source>
+        <translation>建立索引间隔的时间(分钟)</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="393"/>
+        <source>(0 for manual updates only)</source>
+        <translation>(0为手工建立索引)</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="447"/>
+        <source>Skins</source>
+        <translation>皮肤</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="467"/>
+        <source>Select a skin</source>
+        <translation>选择皮肤</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="492"/>
+        <source>Author information</source>
+        <translation>作者信息</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="535"/>
+        <source>Skin preview</source>
+        <translation>预览皮肤</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="558"/>
+        <source>Catalog</source>
+        <translation type="unfinished">目录</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="580"/>
+        <source>File Types</source>
+        <translation type="unfinished">文件类型</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="723"/>
+        <source>+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="743"/>
+        <source>-</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="644"/>
+        <source>Executables</source>
+        <translation>可执行文件</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="699"/>
+        <source>Directories</source>
+        <translation>目录</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="660"/>
+        <source>Depth:</source>
+        <translation>深度:</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="802"/>
+        <source>Rescan Catalog</source>
+        <translation>扫描目录</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="822"/>
+        <source>Indexing 0 items</source>
+        <translation>已找到 0 项</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="857"/>
+        <source>Plugins</source>
+        <translation>插件</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="871"/>
+        <source>Available plugins</source>
+        <translation>有效的插件</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="887"/>
+        <source>Plugin options</source>
+        <translation>插件选项</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="912"/>
+        <source>About Launchy</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="src/options.ui" line="993"/>
+        <source>Launchy&apos;s official homepage is http://www.launchy.net/</source>
+        <translation>Launchy的官方网站 http://www.launchy.net/</translation>
+    </message>
+</context>
+</TS>
